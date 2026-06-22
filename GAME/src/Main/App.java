8577b6f8d5ff5ed730c57579374ee4c05d5bd431
darrene25 <email_github_kamu@example.com>
2026-06-22 10:17:15 +0700

    code terakhir

--- GAME/src/Main/App.java
+++ GAME/src/Main/App.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) {
+        // tes
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
